ab025028ae2995ad23b81e68101ccd979c512326
BALAMURUGAN V <balamuruganvau@outlook.com>
2025-10-06 11:24:00 +0530

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const http=require('http')
 const server = http.createServer((request,response)=>{
-    console.log(request.url)
-    response.end("Hello node.js")
+    if(request.url==="/"){
+        return response.end("THE HOME PAGE")
+    }
+    else if(request.url==="/contact"){
+        return response.end("THE CONTACT PAGE")
+    }
+    else if(request.url==="/about"){
+        return response.end("THE ABOUT PAGE")
+    }
+    else{
+        response.writeHead(404)
+        response.end("PAGE NOT FOUND")
+    }
+    //console.log(request.url)
+    //response.end("Hello node.js")
 })
 server.listen(3000)
